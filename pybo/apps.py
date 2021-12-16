e1f2bd5ea3ea71d6b65748465ad650a272ebb92d
jiin-coder <fpahstiq123@naver.com>
2021-12-16 20:11:36 +0900

    2-02 sqlmigrate pybo 0001 migrate

--- pybo/apps.py
+++ pybo/apps.py
@@ -1,6 +1,12 @@
+# from django.apps import AppConfig
+#
+#
+# class PyboConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'pybo'
+
 from django.apps import AppConfig
 
 
-class PyboConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
+class PyboConfig(AppConfig):  # 장고 버전에 따라 내용이 다를 수 있다.
     name = 'pybo'
